146e88ed36ae454a88b67c12fff5270edaa0db54
rahulfordev <rahulforsms@gmail.com>
2023-12-21 19:11:14 +0600

    virtual data base create

--- api/routes/contact.js
+++ api/routes/contact.js
@@ -1,22 +1,28 @@
 const express = require("express");
 const route = express.Router();
 
+const contacts = [];
+
 route.get("/", (req, res, next) => {
   res.status(200).json({
-    message: "Hello, I'm All contacts",
+    // message: "Hello, I'm All contacts",
+    contacts,
   });
 });
 
 route.post("/", (req, res, next) => {
-  const name = req.body.name;
-  const email = req.body.email;
-
-  console.log(name, email);
-
+  // const name = req.body.name;
+  // const email = req.body.email;
+  // console.log(name, email);
+  contacts.push({
+    name: req.body.name,
+    email: req.body.email,
+  });
   res.status(201).json({
-    message: "This is body parser res",
-    name,
-    email,
+    // message: "This is body parser res",
+    // name,
+    // email,
+    message: "Data Saved",
   });
 });
 
